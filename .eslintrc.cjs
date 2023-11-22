8b8e875bfff480dd35432fa4e91b18f5fd2b8358
Sanket Singh <singhsanket143@gmail.com>
2023-11-22 21:30:17 +0530

    Basic tailwind setup with eslint done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,13 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "simple-import-sort/imports": "error",
+    "semi": [1, "always"]
   },
 }
